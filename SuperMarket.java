199186d27a4533e90dc66f2561972b60186d5382
mohamed anasmalik <mohamedanasasan@gmail.com>
2026-05-27 20:20:24 +0530

    added to the folder

---
+++ SuperMarket.java
@@ -0,0 +1,9 @@
+class SuperMArket{
+	public static void main(String [] args){
+		System.out.println("*****supermarket*******");
+		System.out.println("1	fruits	100");
+		System.out.println("2	milk	30");
+		System.out.println("3	ice		70");
+		System.out.println("*********************");
+	}
+}

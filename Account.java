5fdcd8c989c26acd1175d0ce974d77814e17acc3
anasmalik-786 <mohamedanasasan@gmail.com>
2026-06-25 12:14:57 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,15 @@
+//Create a program to store the details of a bank account holder: account number, customer name, balance, and account type
+
+class Account{
+	public static void main(String[] args){
+		long number=913123243037l;
+		String name="kumar";
+		float balace=1568888.56f;
+		String current="current account";
+		System.out.println("number:"+number);
+		System.out.println("Name"+name);
+		System.out.println("balance:"+balace);
+		System.out.println("Account type:"+current);
+	}
+}	
+		

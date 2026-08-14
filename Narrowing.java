345819ca0db71872843f28053af71c8a571ec8a5
anasmalik-786 <mohamedanasasan@gmail.com>
2026-08-14 15:35:45 +0530

    Add files via upload

---
+++ Narrowing.java
@@ -0,0 +1,10 @@
+//1. Integer to Double Conversion
+class Narrowing{
+	public static void main(String[] args){
+	int a=25;
+	double d=a;
+	
+	System.out.println(d);
+	}
+
+}
---
+++ Widening.java
@@ -0,0 +1,29 @@
+class Widening{
+	public static void main(String[] args){
+	
+	double d = 45.89;
+	int a = (int)d;
+	
+	System.out.println(a);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
+}
